a6053e437c2a92f22367a3d9136df111ac6b7201
kbai010 <kevin.j.bai@pwc.com>
2021-01-12 13:12:04 +0800

    去除敏感文件信息

--- countingDir/mytAdminManageSubs2/mytAdminManageSubs2.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement } from 'lwc';
-import icon_trimble from '@salesforce/resourceUrl/icon_trimble';
-
-export default class MytAdminManageSubs2 extends LightningElement {
-    icon_trimble = icon_trimble;
-}
